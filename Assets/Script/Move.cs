bc1f00134c49245cd2832a5ed2194ec010d14958
nagise237 <61343099+nagise237@users.noreply.github.com>
2020-04-19 22:59:20 +0700

    Add MoveSystem

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody rb;
+
+
+    Vector3 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
